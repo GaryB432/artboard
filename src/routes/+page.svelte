17ff9382f768be9c0fdaae92f725e513825d41bf
Gary Bortosky <garybort@gmail.com>
2023-05-15 13:42:44 -0500

    stub skyline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,22 @@
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<p>
-  <a href="{base}/carnivals">carnivals</a>
-</p>
+<ul>
+  <li>
+    <a href="{base}/carnivals">Random shapes</a>
+  </li>
+  <li>
+    <a href="{base}/skyline">Skyline</a>
+  </li>
+</ul>
 
 <style>
   a {
     text-decoration: underline;
+  }
+  li {
+    background-color: antiquewhite;
+    margin: 1em;
+    padding: 1em;
   }
 </style>
