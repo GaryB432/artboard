025c36eaccf0c61aa27c9508c1f75b1e470e4346
Gary Bortosky <garybort@gmail.com>
2023-05-16 09:41:47 -0500

    add flexbox layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import GithubAnchor from "$lib/components/GithubAnchor.svelte";
+  import { version } from "$app/environment";
   import "../app.css";
 </script>
 
@@ -12,17 +13,22 @@
 <main>
   <slot />
 </main>
-<footer>tbd</footer>
+<footer>{version}</footer>
 
 <style>
   header {
-    padding: 0 1em;
     display: flex;
+    height: 70px;
     justify-content: space-between;
     align-items: center;
-    height: 2em;
   }
+
   main {
-    height: 90vh;
+    flex: auto;
+    /* background-color: #ccc; */
+  }
+
+  footer {
+    height: 25px;
   }
 </style>
