032631eda4d62ed7774cb71613347f5ae96f335d
Gary Bortosky <garybort@gmail.com>
2023-05-17 08:42:29 -0500

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,70 @@
+<script>
+  import GithubAnchor from "./GithubAnchor.svelte";
+</script>
+
+<header>
+  <span class="text-xl">
+    <a href="./">Artboard</a>
+    <span class="label">A web app for fun</span>
+  </span><span class="button-list">
+    <GithubAnchor />
+    <button>Reset to Defaults</button>
+    <button class="active">View Markup</button>
+  </span>
+</header>
+
+<style>
+  header {
+    border-bottom: 1px;
+    border-color: rgb(209, 213, 219);
+    padding: 1rem;
+    height: 4rem;
+    background-color: white;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 10;
+  }
+  .label {
+    margin-left: 0.25rem;
+    padding-bottom: 0.5rem;
+    padding-top: 0.5rem;
+    padding-left: 0.5rem;
+    border: 0 solid rgb(156, 163, 175);
+    border-left-width: 1px;
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    color: rgb(107, 114, 128);
+  }
+  .button-list {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  button {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    cursor: pointer;
+    border-style: none;
+    border-radius: 0.25rem;
+    padding: 0.5rem 1rem;
+    display: inline-block;
+    font-weight: 600;
+    background-color: rgb(243, 244, 246);
+    color: var(--blue);
+  }
+
+  button:hover {
+    background-color: rgb(239, 246, 255);
+  }
+  .active {
+    background-color: var(--blue);
+    color: rgb(243, 244, 246);
+  }
+
+  .active:hover {
+    background-color: var(--blue);
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,18 @@
 <script>
-  import GithubAnchor from "$lib/components/GithubAnchor.svelte";
   import { version } from "$app/environment";
+  import Header from "$lib/components/Header.svelte";
   import "../app.css";
 </script>
 
-<header>
-  <div>
-    <a href="./">artboard</a>
-  </div>
-  <GithubAnchor />
-</header>
+<Header />
 <main>
   <slot />
 </main>
 <footer>{version}</footer>
 
 <style>
-  header {
-    display: flex;
-    height: 70px;
-    justify-content: space-between;
-    align-items: center;
-  }
-
   main {
     flex: auto;
-    /* background-color: #ccc; */
   }
 
   footer {
