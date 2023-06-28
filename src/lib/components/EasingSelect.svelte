591e9f3264cf5ab948f53b19c0b6692bee9d7e21
Gary Bortosky <garybort@gmail.com>
2023-06-28 09:59:43 -0500

    remove unused variable

--- src/lib/components/EasingSelect.svelte
+++ src/lib/components/EasingSelect.svelte
@@ -27,7 +27,6 @@
 <style>
   select {
     border-style: solid;
-    --gb-ring-inset: var(--tw-empty);
     font-family: inherit;
     appearance: none;
     background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGZpbGw9J25vbmUnIHZpZXdCb3g9JzAgMCAyMCAyMCc+PHBhdGggc3Ryb2tlPScjNmI3MjgwJyBzdHJva2UtbGluZWNhcD0ncm91bmQnIHN0cm9rZS1saW5lam9pbj0ncm91bmQnIHN0cm9rZS13aWR0aD0nMS41JyBkPSdNNiA4bDQgNCA0LTQnLz48L3N2Zz4=);
