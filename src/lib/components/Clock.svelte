cf64ba8b7a1f688312e6d6defd7c895e37a9f6a1
Gary Bortosky <garybort@gmail.com>
2023-08-18 13:52:46 -0500

    add wipe

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -3,17 +3,25 @@
   import { onMount } from "svelte";
 
   let container: HTMLElement | null = null;
+  let wiping = false;
   const c = new Clock();
   onMount(() => {
     c.init(container, 400);
     c.start();
   });
   function drawRandoFrame() {}
-  function wipe() {}
+  function wipe() {
+    if (container) {
+      wiping = true;
+      setTimeout(() => {
+        wiping = false;
+      }, 2000);
+    }
+  }
 </script>
 
 <section>
-  <div class="container" bind:this={container} />
+  <div class="container" bind:this={container} class:wiping />
   <nav class="button-bar">
     <button on:click={drawRandoFrame}>Go</button>
     <button on:click={wipe}>Wipe</button>
@@ -38,9 +46,16 @@
     /* height: fit-content; */
 
     padding: 6px;
+    /* transform: scale(1); */
     border: 1em solid var(--blue);
     background-color: #8cb7f2;
     border-radius: 50%;
+    transition: all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+
+  .container.wiping {
+    transform: scale(1.2) translate(-100px, 15px);
+    /* background-color: orange; */
   }
 
   .button-bar {
