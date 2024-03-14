585d3d92d00cad95addb97d781c8c6ad524a7909
Gary Bortosky <garybort@gmail.com>
2024-03-14 13:56:42 -0500

    update home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,64 @@
 <section class="twofer">
   <ul>
     <li>
-      <a href="{base}/carnivals">Random shapes</a>
+      <a href="{base}/boids">boids</a>
+      <div>not yet boids</div>
+      <div class="tech"></div>
     </li>
     <li>
-      <a href="{base}/skyline">Skyline</a>
+      <a href="{base}/carnivals">carnivals</a>
+      <div>random shapes</div>
+      <div class="tech"></div>
     </li>
     <li>
-      <a href="{base}/three/cuber">ThreeJS Cube</a>
+      <a href="{base}/cloth">cloth</a>
+      <div>tearable cloth</div>
+      <div class="tech"></div>
     </li>
     <li>
-      <a href="{base}/three/cinematic">ThreeJS Cinematic Camera</a>
+      <a href="{base}/radu/vectors">radu/vectors</a>
+      <div>vector math and visualization from radu</div>
+      <div class="tech"></div>
     </li>
     <li>
-      <a href="{base}/timeline">Timeline Canvas</a>
+      <a href="{base}/radu/virtual-world">radu/virtual-world</a>
+      <div>virtual world demo from radu's machine learning course</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/siner">siner</a>
+      <div>basic sine wave</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/skyline">skyline</a>
+      <div>skyline</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/stilo">stilo</a>
+      <div>basic points</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/three/cinematic">three/cinematic</a>
+      <div>threejs cinematic camera</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/three/cuber">three/cuber</a>
+      <div>threejs spinning cube basics</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/timeline">timeline</a>
+      <div>scrollable infinite timeline</div>
+      <div class="tech"></div>
+    </li>
+    <li>
+      <a href="{base}/virtual-world">virtual-world</a>
+      <div>basic</div>
+      <div class="tech"></div>
     </li>
   </ul>
   <section>
@@ -45,6 +90,8 @@
     /* width: 70vw; */
   }
   li {
+    display: flex;
+    gap: 1rem;
     background-color: antiquewhite;
     margin: 0.5em 0;
     padding: 0.5em;
