4aaa09cc8880e21e4752c6e05c83b8a509f5337d
Gary Bortosky <garybort@gmail.com>
2024-03-16 23:43:25 -0500

    stub graph

--- src/routes/virtual-world/+page.ts
+++ src/routes/virtual-world/+page.ts
@@ -1,9 +1,16 @@
 import type { PageLoad } from "./$types";
-import type { Point, Segment } from "./world/primitives";
+import { Point, Segment } from "./world/primitives";
 
 export const load = (async ({ params, fetch }) => {
-  const points: Point[] = [];
-  const segments: Segment[] = [];
+  const points: Point[] = [
+    new Point(10, 10),
+    new Point(20, 20),
+    new Point(30, 20),
+  ];
+  const segments: Segment[] = [
+    new Segment(points[0], points[1]),
+    new Segment(points[1], points[2]),
+  ];
 
   return {
     points,
