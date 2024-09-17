76f1215a42287feb7073d360ef39ecab65c6bebc
Gary Bortosky <garybort@gmail.com>
2024-09-17 14:35:12 -0500

    remove broken cinematic camera

--- src/routes/three/cinematic/+page.svelte
+++ src/routes/three/cinematic/+page.svelte
@@ -1,28 +1,8 @@
-<!-- <script lang="ts">
-</script>
-
-<svelte:head>
-  <meta
-    name="viewport"
-    content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-  />
-
-  <title>three.js webgl - camera cinematic</title>
-</svelte:head> -->
-<script lang="ts">
-  import { createScene } from "$lib/cinematic/scene";
-  import { onMount } from "svelte";
-
-  let el: HTMLCanvasElement;
-
-  onMount(() => {
-    createScene(el);
-  });
-</script>
-
 <svelte:head>
   <title>Three.js Sveltekit</title>
   <meta name="description" content="Three.js example app built with Svelte" />
 </svelte:head>
 
-<canvas bind:this={el}></canvas>
+<main>
+  <h1>returning soon</h1>
+</main>
