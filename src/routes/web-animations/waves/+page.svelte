fda0a6d73a72ae761a36da747886683c5c703f8c
Gary Bortosky <garybort@gmail.com>
2024-09-24 12:24:04 -0500

    working out waves

--- src/routes/web-animations/waves/+page.svelte
+++ src/routes/web-animations/waves/+page.svelte
@@ -1,33 +1,129 @@
 <script lang="ts">
-  let data = $state({ subject: "waves" });
+  import { onMount } from "svelte";
+
+  class Box {
+    width = $state(0);
+    height = $state(0);
+    div: Animatable | null = $state(null);
+    constructor(x: number, y: number) {
+      this.width = x;
+      this.height = y;
+    }
+    animate(
+      keyframes: Keyframe[] | PropertyIndexedKeyframes | null,
+      options?: number | KeyframeAnimationOptions,
+    ): Animation | null {
+      if (this.div) {
+        return this.div.animate(keyframes, options);
+      }
+      console.log("fall thru!");
+      return null;
+    }
+  }
+
+  let boxes: Box[] = $state([]);
+
+  var sideCount = 20;
+
+  let container: HTMLDivElement;
+  var adjustment = $derived((sideCount % 2) * 0.5);
+  var min = $derived(-sideCount / 2 + adjustment);
+  var max = $derived(sideCount / 2 + adjustment);
+
+  function loadBoxes() {
+    for (var y = min; y < max; y++) {
+      for (var x = min; x < max; x++) {
+        boxes.push(new Box(x, y));
+      }
+    }
+  }
+
+  loadBoxes();
+
+  onMount(() => {
+    container.animate(
+      {
+        transform: [
+          "rotateX(70deg) rotateZ(0deg)",
+          "rotateX(70deg) rotateZ(360deg)",
+        ],
+      },
+      {
+        duration: 20000,
+        iterations: Infinity,
+      },
+    );
+
+    for (const box of boxes) {
+      box.animate(
+        {
+          transform: ["translateZ(0px)", "translateZ(40px)"],
+          opacity: [1, 0],
+        },
+        {
+          delay: (box.width * box.width + box.height * box.height) * 20,
+          duration: 2000,
+          iterations: Infinity,
+          direction: "alternate",
+          easing: "ease-in",
+        },
+      );
+    }
+  });
+
+  // function createBox() {
+  //   // var box = document.createElement("div");
+  //   const box = new Box();
+  //   // box.className = "box";
+  //   box.width = 100 / sideCount;
+  //   box.height = 100 / sideCount;
+  //   // container.appendChild(box);
+  //   boxes.push(box);
+  //   // return box;
+  // }
 </script>
 
-<svelte:head>
-  <title>artboard - waves</title>
-</svelte:head>
-
-<article class="container">
-  {data.subject} is coming some day soon
-</article>
+<div class="host">
+  <div id="perspective">
+    <div bind:this={container} id="container">
+      {#each boxes as box}
+        <div
+          class="box"
+          bind:this={box.div}
+          style="width: {box.width}%; height: {box.width}%"
+        ></div>
+      {/each}
+    </div>
+  </div>
+</div>
 
 <style>
-  .container {
-    padding: 1em;
-    border: thin solid silver;
+  /* body {
+    } */
+
+  .host {
+    margin: 0;
+    background: blue;
+    overflow: hidden;
   }
-  @media screen and (min-width: 576px) {
-    /* landscape phones */
+
+  #perspective {
+    margin-left: calc((100vw - 50vmin) / 2);
+    margin-top: calc((100vh - 50vmin) / 4);
+    width: 50vmin;
+    height: 50vmin;
+    perspective: 60vmin;
   }
-  @media screen and (min-width: 768px) {
-    /* tablets */
+
+  #container {
+    width: 50vmin;
+    height: 50vmin;
+    line-height: 0;
+    transform-style: preserve-3d;
   }
-  @media screen and (min-width: 992px) {
-    /* desktops */
-  }
-  @media screen and (min-width: 1200px) {
-    /* large desktops */
-  }
-  @media screen and (min-width: 1400px) {
-    /* larger desktops */
+
+  .box {
+    display: inline-block;
+    background: black;
   }
 </style>
