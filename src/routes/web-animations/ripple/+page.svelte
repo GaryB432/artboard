a15dc442b2a6f1d00a65b5fb679ee1c8d4c949c3
Gary Bortosky <garybort@gmail.com>
2024-09-24 10:23:22 -0500

    stub web-animation routes

---
+++ src/routes/web-animations/ripple/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let data = $state({ subject: "ripple" });
+</script>
+
+<svelte:head>
+  <title>artboard - ripple</title>
+</svelte:head>
+
+<article class="container">
+  {data.subject} works
+</article>
+
+<style>
+  .container {
+    padding: 1em;
+    border: thin solid silver;
+  }
+  @media screen and (min-width: 576px) {
+    /* landscape phones */
+  }
+  @media screen and (min-width: 768px) {
+    /* tablets */
+  }
+  @media screen and (min-width: 992px) {
+    /* desktops */
+  }
+  @media screen and (min-width: 1200px) {
+    /* large desktops */
+  }
+  @media screen and (min-width: 1400px) {
+    /* larger desktops */
+  }
+</style>
