f7ed2afc4c4a6c0699efbf8e6c9cd15022ccb6c7
Gary Bortosky <garybort@gmail.com>
2024-09-25 21:11:49 -0500

    clean up route page

--- src/routes/web-animations/parallax-starfield/+page.svelte
+++ src/routes/web-animations/parallax-starfield/+page.svelte
@@ -1,110 +1,54 @@
 <script lang="ts">
-  // import Layer from "./components/Layer.svelte";
-  import LayerPanel from "./components/Layer.svelte";
-  import StarPanel from "./components/Star.svelte";
-  import { Layer, Star } from "./shared";
-  var layerCount = 5;
-  var starCount = 100;
-  // var universe = document.getElementById("universe");
+  import { createUniverse } from "./shared.svelte";
+  import StarfieldPanel from "./components/Starfield.svelte";
+  import WebAnimationsHome from "../WebAnimationsHome.svelte";
+  import { onMount } from "svelte";
+  import type { Vector2Tuple } from "three";
 
-  let universe: HTMLElement | null = $state(null);
-  // let layerComponents: LayerPanel[] = $state([]);
-  let layers: Layer[] = $state([]);
+  let size: Vector2Tuple = $state([0, 0]);
 
-  // var w = window;
-  // var d = document;
-  // var e = d.documentElement;
-  // var g = d.getElementsByTagName("body")[0];
-  // var width = w.innerWidth || e.clientWidth || g.clientWidth;
-  // var height = w.innerHeight || e.clientHeight || g.clientHeight;
-  for (var l = 0; l < layerCount; ++l) {
-    //   var layer = document.createElement("div");
-    const layer = new Layer();
-    // layers.push(new Layer());
-    //   layer.setAttribute("id", "layer" + l);
-    //   layer.setAttribute("class", "starfield");
-    //   universe.appendChild(layer);
-    for (var i = 0; i < starCount; ++i) {
-      // var xpos = Math.round(Math.random() * width);
-      // var ypos = Math.round(Math.random() * height);
-      for (var s = 0; s < 2; ++s) {
-        //       var star = document.createElement("div");
-        const star = new Star();
-        //       star.setAttribute("class", "star" + l);
-        //       star.style.webkitTransform =
-        //         "translate(" + (xpos + s * width) + "px, " + ypos + "px)";
-        //       star.style.transform =
-        //         "translate(" + (xpos + s * width) + "px, " + ypos + "px)";
-        //       star.style.backgroundColor = "white"; //rgb(' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ')';
-        //       layer.appendChild(star);
-        console.log(star);
-        layer.stars.push(star);
-      }
-      layers.push(layer)
+  let container: HTMLDivElement | null = $state(null);
+
+  let universe = $derived(createUniverse(size, 5, 200));
+
+  onMount(() => {
+    if (container) {
+      const a = container.getBoundingClientRect();
+      size = [a.width, a.height];
     }
-    //   layer.animate(
-    //     [
-    //       { transform: "translate3d(0, 0, 0)" },
-    //       { transform: "translate3d(-" + width + "px, 0, 0)" },
-    //     ],
-    //     { duration: Math.pow(2, layerCount - l) * 1000, iterations: Infinity },
-    //   );
-  }
+    // $inspect(universe);
+  });
 </script>
 
-<div bind:this={universe} id="xxxxx-universe">
-  {#each layers as layer}
-  {layer.stars.length}
-    <LayerPanel {...layer}></LayerPanel>
-  {/each}
+<svelte:head>
+  <title>artboard - parallax-starfield</title>
+</svelte:head>
+
+<div class="container" bind:this={container}>
+  <div id="universe">
+    {#each universe.starfields as starfield}
+      <StarfieldPanel
+        {starfield}
+        width={size[0]}
+        universeDepth={universe.starfields.length}
+      ></StarfieldPanel>
+    {/each}
+  </div>
 </div>
 
+<WebAnimationsHome></WebAnimationsHome>
+
 <style>
-  /* body {
-    background-color: black;
+  .container {
+    width: 100vw;
+    height: 80vh;
+    border: thin solid silver;
     overflow: hidden;
-  } */
-
-  .starfield {
-    background-color: rgba(0, 0, 0, 0);
   }
-
-  .star0 {
-    height: 1px;
-    width: 1px;
-    opacity: 1;
-    position: absolute;
-  }
-
-  .star1 {
-    height: 2px;
-    width: 2px;
-    border-radius: 50%;
-    opacity: 1;
-    position: absolute;
-  }
-
-  .star2 {
-    height: 3px;
-    width: 3px;
-    border-radius: 50%;
-    opacity: 1;
-    position: absolute;
-  }
-
-  .star3 {
-    height: 4px;
-    width: 4px;
-    border-radius: 50%;
-    opacity: 1;
-    position: absolute;
-  }
-
-  .star4 {
-    height: 4px;
-    width: 4px;
-    border-radius: 50%;
-    opacity: 1;
-    position: absolute;
+  #universe {
+    position: relative;
+    background-color: black;
+    height: 100%;
+    width: 100%;
   }
 </style>
