36c7306996b3ebfc9383478d88133ed24b338e01
Gary Bortosky <garybort@gmail.com>
2024-10-09 14:51:27 -0500

    decent ryg

--- src/routes/traffic-signal/+page.svelte
+++ src/routes/traffic-signal/+page.svelte
@@ -15,21 +15,49 @@
   }
 
   onMount(() => {
-    const ccs = ["red", "yellow", "green"];
-    const lenses = ccs.map((fill) => ({ stroke: "none", fill }));
-    const r = 12;
-    const gap = 3;
+    if (!svg) return;
+    // const ss = new Vector(svg.clientWidth, svg.clientHeight);
+    // console.log(ss);
+
+    // const cy = svg.clientHeight / 2;
+
+    const center = new Vector(
+      svg.width.baseVal.value,
+      svg.height.baseVal.value,
+    ).multiplyScaler(0.5);
+
+    console.log(center);
+
+    const lenses = ["red", "yellow", "green"].map((fill) => ({
+      stroke: "black",
+      strokeWidth: "1",
+      fill,
+    }));
+    const r = 100;
+    const gap = 30;
+
+    // const cir = createCircle(new Vector(0, 0), r, {
+    //   stroke: "black",
+    //   strokeWidth: "1",
+    //   fill: "blue",
+    // });
+    // svg.appendChild(cir);
 
     for (let i = 0; i < lenses.length; i++) {
-      const nv = new Vector(50, (i + 1) * 50);
+      // console.log(i, cy);
+      const dd = 2 * r + gap;
+      // const nv = new Vector(200, dd * (i - 1));
+
+      const nv = new Vector(0, dd * (i - 1)).add(center);
+
       const c = createCircle(nv, r, lenses[i]);
-      svg?.appendChild(c);
+      svg.appendChild(c);
     }
   });
 </script>
 
 <div class="container">
-  <svg viewBox="0 0 108 182" bind:this={svg}></svg>
+  <svg height="1080" width="1920" viewBox="0 0 1920 1080" bind:this={svg}></svg>
 </div>
 
 <button onclick={scaleInnerCircle}>Scale Inner Circle</button>
@@ -44,11 +72,13 @@
     display: flex;
     justify-content: center;
     width: 100%;
+    border: thin solid green;
   }
 
   svg {
     border: thin solid silver;
-    height: 600px;
-    width: 1000px;
+    width: auto;
+    height: 60vh;
+    transform-origin: 0 0;
   }
 </style>
