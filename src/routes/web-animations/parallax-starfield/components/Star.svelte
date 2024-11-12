6e7edeb6edf264f01c94662c87bbdcfd0b1f03f9
Gary Bortosky <garybort@gmail.com>
2024-11-11 20:11:41 -0600

    Add parallax starfield (#19)
    * add components
    
    * clean up route page
    
    * add parallax structure
    
    * clean up pr

--- src/routes/web-animations/parallax-starfield/components/Star.svelte
+++ src/routes/web-animations/parallax-starfield/components/Star.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-  let subject = $state("Star component");
+  import type { Star } from "../shared.svelte";
+
+  type Props = {
+    star: Star;
+  };
+
+  let { star }: Props = $props();
+  let [x, y, z] = $derived(star.position);
 </script>
 
-<span class="container">*</span>
+<div class="star" style="--distance: {z + 1}; top: {y}px; left: {x}px;"></div>
 
 <style>
-  .container {
-    border: none;
+  .star {
+    background-color: white;
+    height: calc(var(--distance) * 1px);
+    width: calc(var(--distance) * 1px);
+    border-radius: 50%;
+    opacity: 1;
+    position: absolute;
   }
 </style>
