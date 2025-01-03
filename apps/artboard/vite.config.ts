4e2d188ae52bb30b97332f11c0f3cfded35fea40
GaryB432 <garybort@gmail.com>
2025-01-03 16:01:07 -0600

    update namesake

--- apps/artboard/vite.config.ts
+++ apps/artboard/vite.config.ts
@@ -1,10 +1,10 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    environment: "happy-dom",
-    include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
+	plugins: [sveltekit()],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
