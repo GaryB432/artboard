688b71c06875332af7406e910eef86fc775330af
GaryB432 <garybort@gmail.com>
2025-01-05 10:35:17 -0600

    add fireship images

--- apps/artboard/src/lib/scenes/background.ts
+++ apps/artboard/src/lib/scenes/background.ts
@@ -113,12 +113,12 @@ Array(200).fill(undefined).forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+const spaceTexture = new THREE.TextureLoader().load("../fireship/space.jpg");
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load("jeff.png");
+const jeffTexture = new THREE.TextureLoader().load("../fireship/jeff.png");
 
 const jeff = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
@@ -129,8 +129,8 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+const moonTexture = new THREE.TextureLoader().load("../fireship/moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("../fireship/normal.jpg");
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
