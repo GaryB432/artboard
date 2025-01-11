95bfc9824f011822127d2fcbfcf6046150abd2ff
Gary Bortosky <garybort@gmail.com>
2025-01-11 15:31:02 -0600

    stub click handler

--- apps/hvac/src/main.ts
+++ apps/hvac/src/main.ts
@@ -26,6 +26,15 @@ class Scene {
   private zoom = 1;
   private svgSize: Point;
   private worldSize = new Point(4000, 3000);
+  private mouse = {
+    down: false,
+    button: 1,
+    x: 0,
+    y: 0,
+    px: 0,
+    py: 0,
+  };
+
   constructor(private svg: SVGSVGElement) {
     this.svgSize = new Point(
       this.svg.width.baseVal.value,
@@ -42,6 +51,29 @@ class Scene {
         `0 0 ${400 * this.zoom} ${300 * this.zoom}`,
       );
       evt.preventDefault();
+    });
+    svg.addEventListener("click", (evt) => {
+      const bcr = svg.getBoundingClientRect();
+      const dot = document.createElementNS(svgns, "circle");
+      const cx = evt.clientX - bcr.left - 2;
+      const cy = evt.clientY - bcr.top - 2;
+      const spot = new Point(cx,cy)
+      dot.setAttributeNS(null, "cx", spot.x.toString());
+      dot.setAttributeNS(null, "cy", spot.y.toString());
+      dot.setAttributeNS(null, "r", "4");
+      dot.setAttributeNS(null, "fill", "blue");
+      svg.append(dot);
+      console.log(spot);
+      // const dir = Math.sign(evt.deltaY);
+      // const step = 0.2;
+      // this.zoom += dir * step;
+      // this.zoom = Math.max(1, Math.min(5, this.zoom));
+      // this.svg.setAttributeNS(
+      //   null,
+      //   "viewBox",
+      //   `0 0 ${400 * this.zoom} ${300 * this.zoom}`,
+      // );
+      // evt.preventDefault();
     });
   }
   reset() {
