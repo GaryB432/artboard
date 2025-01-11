54195cfcefda8e04826c2134a02d306a72b67eac
Gary Bortosky <garybort@gmail.com>
2025-01-11 11:21:44 -0600

    free up shared library root

--- apps/artboard/svelte.config.js
+++ apps/artboard/svelte.config.js
@@ -8,7 +8,7 @@ const config = {
   kit: {
     adapter: adapter(),
     alias: {
-      "@libraries/radu": "../../libraries/radu",
+      "@libraries/radu": "../../libraries/radu/src",
     },
   },
 };
