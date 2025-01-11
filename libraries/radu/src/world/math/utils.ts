71393e1242b84a3b613e6b60001069e302d972ad
Gary Bortosky <garybort@gmail.com>
2025-01-11 11:35:19 -0600

    Move to src (#28)
    * free up shared library root
    
    * specify array types

--- libraries/radu/src/world/math/utils.ts
+++ libraries/radu/src/world/math/utils.ts
@@ -7,7 +7,7 @@ export function getNearestPoint(
   threshold = Number.MAX_SAFE_INTEGER,
 ): Point | null {
   let minDist = Number.MAX_SAFE_INTEGER;
-  let nearest = null;
+  let nearest: Point | null = null;
   for (const point of points) {
     const dist = distance(point, loc);
     if (dist < minDist && dist < threshold) {
@@ -24,7 +24,7 @@ export function getNearestSegment(
   threshold = Number.MAX_SAFE_INTEGER,
 ): Segment | null {
   let minDist = Number.MAX_SAFE_INTEGER;
-  let nearest = null;
+  let nearest: Segment | null = null;
   for (const seg of segments) {
     const dist = seg.distanceToPoint(loc);
     if (dist < minDist && dist < threshold) {
