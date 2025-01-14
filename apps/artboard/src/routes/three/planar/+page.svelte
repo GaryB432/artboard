c4192547c4d0b6c9483a2bfbccd5731e5bb18c98
Gary Bortosky <garybort@gmail.com>
2025-01-14 13:54:13 -0600

    basics

--- apps/artboard/src/routes/three/planar/+page.svelte
+++ apps/artboard/src/routes/three/planar/+page.svelte
@@ -1,1 +1,15 @@
-soon
+<script lang="ts">
+  import { onMount } from "svelte";
+ 
+  let el: HTMLCanvasElement;
+
+  onMount(() => {
+    createScene(el);
+  });
+
+  function createScene(el: HTMLCanvasElement) {
+    throw new Error("Function not implemented.");
+  }
+</script>
+
+<canvas bind:this={el}> </canvas>
---
+++ apps/artboard/src/routes/three/planar/scene.ts
@@ -0,0 +1,161 @@
+import * as THREE from "three";
+
+let renderer: THREE.Renderer;
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera();
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
+const resize = () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight - 100;
+  renderer.setSize(w, h);
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+};
+
+export function createScene(el: HTMLCanvasElement): void {
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
+  resize();
+  animate();
+}
+
+window.addEventListener("resize", resize);
+
+/*
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import "./style.css";
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(10, 15, -22);
+
+orbit.update();
+
+const planeMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    visible: false,
+  }),
+);
+planeMesh.rotateX(-Math.PI / 2);
+scene.add(planeMesh);
+
+const grid = new THREE.GridHelper(20, 20);
+scene.add(grid);
+
+const highlightMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1),
+  new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    transparent: true,
+  }),
+);
+highlightMesh.rotateX(-Math.PI / 2);
+highlightMesh.position.set(0.5, 0, 0.5);
+scene.add(highlightMesh);
+
+const mousePosition = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+let intersects;
+
+window.addEventListener("mousemove", function (e) {
+  mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+  mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mousePosition, camera);
+  intersects = raycaster.intersectObject(planeMesh);
+  if (intersects.length > 0) {
+    const intersect = intersects[0];
+    const highlightPos = new THREE.Vector3()
+      .copy(intersect.point)
+      .floor()
+      .addScalar(0.5);
+    highlightMesh.position.set(highlightPos.x, 0, highlightPos.z);
+
+    const objectExist = objects.find(function (object) {
+      return (
+        object.position.x === highlightMesh.position.x &&
+        object.position.z === highlightMesh.position.z
+      );
+    });
+
+    if (!objectExist) highlightMesh.material.color.setHex(0xffffff);
+    else highlightMesh.material.color.setHex(0xff0000);
+  }
+});
+
+const sphereMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(0.4, 4, 2),
+  new THREE.MeshBasicMaterial({
+    wireframe: true,
+    color: 0xffea00,
+  }),
+);
+
+const objects: THREE.Mesh<THREE.BufferGeometry, THREE.Material>[] = [];
+
+window.addEventListener("mousedown", function () {
+  const objectExist = objects.find(function (object) {
+    return (
+      object.position.x === highlightMesh.position.x &&
+      object.position.z === highlightMesh.position.z
+    );
+  });
+
+  if (!objectExist) {
+    if (intersects.length > 0) {
+      const sphereClone = sphereMesh.clone();
+      sphereClone.position.copy(highlightMesh.position);
+      scene.add(sphereClone);
+      objects.push(sphereClone);
+      highlightMesh.material.color.setHex(0xff0000);
+    }
+  }
+  console.log(scene.children.length);
+});
+
+function animate(time: number): void {
+  highlightMesh.material.opacity = 1 + Math.sin(time / 120);
+  objects.forEach(function (object) {
+    object.rotation.x = time / 1000;
+    object.rotation.z = time / 1000;
+    object.position.y = 0.5 + 0.5 * Math.abs(Math.sin(time / 1000));
+  });
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+*/
