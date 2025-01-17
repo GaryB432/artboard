f96656b16041ba1530c96f99aca98fc0552b0dc6
Gary Bortosky <garyb432@users.noreply.github.com>
2025-01-17 09:44:38 -0600

    tweak yellow light

--- apps/artboard/src/routes/traffic-signal/+page.svelte
+++ apps/artboard/src/routes/traffic-signal/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { createCircle, createRect } from "@libraries/graphics/svg/element";
-  
+
   import { Vector } from "$lib/vector/vector";
   import { onMount } from "svelte";
 
@@ -42,21 +42,41 @@
     // });
     // svg.appendChild(cir);
 
-    for (let i = 0; i < 3; i++) {
-      // console.log(i, cy);
-      const dd = 2 * r + gap;
-      // const nv = new Vector(200, dd * (i - 1));
+    // for (let i = 0; i < 3; i++) {
+    //   // console.log(i, cy);
+    //   const dd = 2 * r + gap;
+    //   // const nv = new Vector(200, dd * (i - 1));
 
-      const nv = new Vector(0, (2 * r + gap) * (i - 1)).add(center);
+    //   const nv = new Vector(0, (2 * r + gap) * (i - 1)).add(center);
 
-      const c = createCircle(nv, r, lenses[i]);
-      svg.appendChild(c);
-    }
+    //   const c = createCircle(nv, r, lenses[i]);
+    //   c.setAttributeNS(null, "r", grow ? "34" : "60");
+    //   svg.appendChild(c);
+    // }
   });
 </script>
 
 <div class="container">
-  <svg width="1920" height="1080" viewBox="0 0 1920 1080" bind:this={svg}></svg>
+  <svg width="1920" height="1080" viewBox="0 0 1920 1080" bind:this={svg}>
+    <circle cx="960" cy="220" r="150" stroke="black" stroke-width="1" fill="red"
+    ></circle>
+    <circle
+      cx="960"
+      cy="540"
+      r={grow ? "300" : "150"}
+      stroke="black"
+      stroke-width="1"
+      fill="yellow"
+    ></circle>
+    <circle
+      cx="960"
+      cy="860"
+      r="150"
+      stroke="black"
+      stroke-width="1"
+      fill="green"
+    ></circle>
+  </svg>
 </div>
 
 <button onclick={scaleInnerCircle}>Scale Inner Circle</button>
