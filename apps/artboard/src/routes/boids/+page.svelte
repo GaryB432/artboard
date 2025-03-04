05a34bed422b8fce263cb231e89afb44f4bd65a1
Gary Bortosky <garyb432@users.noreply.github.com>
2025-03-03 20:48:48 -0600

    update boundary

--- apps/artboard/src/routes/boids/+page.svelte
+++ apps/artboard/src/routes/boids/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { getRandomElement } from "$lib/utils/misc";
-  import { type Segment, makeRectangle } from "@artboard/graphics";
+  import { type Rectangle, makeRectangle } from "@artboard/graphics";
   import { Vector } from "@artboard/vector";
   import { Tween } from "svelte/motion";
 
@@ -13,65 +13,42 @@
     pos: Tween<Vector>;
     rad: number;
     speed: number;
+    to: Vector;
+    edge?: EdgeName;
   }
 
   const margin = 10;
   const maxSpeed = 1000;
 
-  const solidDOMRect = new DOMRect(0, 0, 400, 300);
+  const basisRect = new DOMRect(0, 0, 400, 300);
 
-  let thatRect = $derived(
-    new DOMRect(
-      solidDOMRect.x + margin,
-      solidDOMRect.y + margin,
-      solidDOMRect.width - margin * 2,
-      solidDOMRect.height - margin * 2,
-    ),
+  let container = new DOMRect(
+    basisRect.x + margin,
+    basisRect.y + margin,
+    basisRect.width - margin * 2,
+    basisRect.height - margin * 2,
   );
 
-  let rectCenter = $derived(
-    new Vector(
-      thatRect.left + thatRect.width / 2,
-      thatRect.top + thatRect.height / 2,
-    ),
+  let rectCenter = new Vector(
+    container.left + container.width / 2,
+    container.top + container.height / 2,
   );
-
-  const rectSegs = $derived(makeRectangle(thatRect));
-
-  let qbs = new Array(1).fill(null).map<Boid>((_, i) => {
-    return {
-      pos: new Tween<Vector>(rectCenter.clone()),
-      speed: 1,
-      rad: 5,
-    };
-  });
-
-  let currentBoundary: Segment = {
-    from: new Vector(0, 0),
-    to: new Vector(0, 0),
-  };
 
   const greenSegw: TweenedSegment = $derived({
     from: new Tween<Vector>(rectCenter.clone(), { duration: 200 }),
     to: new Tween<Vector>(rectCenter.clone(), { duration: 200 }),
   });
 
-  // console.log(rectSegs);
-
-  let boids: Boid[] = $state(qbs);
-
-  function segmentMag(segment: Segment): number {
-    return segment.from.distanceTo(segment.to);
-  }
-
-  function segmentEq(a: Segment, b: Segment): boolean {
-    return (
-      a.from.x === b.from.x &&
-      a.to.x === b.to.x &&
-      a.from.y === b.from.y &&
-      a.to.y === b.to.y
-    );
-  }
+  let boids: Boid[] = $state(
+    new Array(3).fill(null).map<Boid>((_, i) => {
+      return {
+        pos: new Tween<Vector>(rectCenter.clone()),
+        rad: Math.random() * 15 + 5,
+        speed: 1,
+        to: rectCenter.clone(),
+      };
+    }),
+  );
 
   function getRandomPointOnLine(s: { from: Vector; to: Vector }) {
     const t = Math.random();
@@ -83,66 +60,68 @@
     return new Vector(x, y);
   }
 
-  // // Example usage:
-  // const linePoint1 = { x: 1, y: 1 };
-  // const linePoint2 = { x: 5, y: 4 };
+  type EdgeName = keyof Rectangle;
 
-  // const randomPoint = getRandomPointOnLine(linePoint1, linePoint2);
-  // console.log(randomPoint);
+  // const wallGenerator = nextWallGenerator(["top", "right", "bottom", "left"]);
 
-  function randomOne() {
-    const boid = boids[0];
+  async function advance(): Promise<void> {
+    // return new Promise((a, b) => {
+    //   a();
+    // });
+    // const boid = boids[0];
 
-    const boxSegs = Object.values(rectSegs);
+    boids.forEach((boid) => {
+      // let nw: IteratorResult<keyof Rectangle, void>;
+      // do {
+      //   nw = wallGenerator.next();
+      // } while (!nw.done && nw.value === boid.edge);
 
-    const pointsOnBox = boxSegs.map<Vector>((s) => {
-      if (s.from.x === s.to.x) {
-        return new Vector(s.from.x, Math.random() * segmentMag(s) + margin);
-      }
-      return new Vector(Math.random() * segmentMag(s) + margin, s.from.y);
-    });
+      // if (nw.done) {
+      //   throw new Error("should not run out");
+      // }
 
-    const from = boid.pos.current;
+      // boid.edge = nw.value!;
 
-    const nb = () => {
-      const el = getRandomElement(
-        boxSegs.filter((b) => !segmentEq(currentBoundary, b)),
+      let nextEdge: EdgeName;
+
+      nextEdge = getRandomElement(
+        ["top", "right", "bottom", "left"].filter(
+          (g) => g !== boid.edge,
+        ) as EdgeName[],
       );
-      return el;
-    };
 
-    let nextBoundary: Segment = nb();
-    console.log(
-      JSON.stringify({ nextBoundary, currentBoundary }, undefined, 2),
-    );
-    if (segmentEq(nextBoundary, currentBoundary)) {
-      throw new Error("nope");
-    }
+      boid.edge = nextEdge;
 
-    // if (s)
+      let boundaryRect = makeRectangle(container, boid.rad);
 
-    const p2 = getRandomPointOnLine(nextBoundary);
+      let currentBoundary = boundaryRect[boid.edge];
 
-    const to = p2;
+      // const from = boid.pos.current;
 
-    // let to = getRandomElement(pointsOnBox);
+      boid.to = getRandomPointOnLine(currentBoundary);
 
-    const motion: Segment = { from, to };
-
-    console.log(segmentMag(motion));
-
-    const m = segmentMag(motion);
-    const d = { from: motion.from, to: motion.to.scale(9) };
-
-    boid.pos.set(motion.to, { duration: boid.speed * maxSpeed });
-    boid.speed -= 0.01;
-    greenSegw.from.set(motion.from, { duration: 0 }).then(() => {
-      greenSegw.to.set(motion.to, { duration: 0 });
-      currentBoundary = nextBoundary;
+      if (
+        boid.to.distanceTo(currentBoundary.from) < boid.rad ||
+        boid.to.distanceTo(currentBoundary.to) < boid.rad
+      ) {
+        console.warn("bounds");
+      }
     });
+
+    const mots = boids.map((b) => b.pos.set(b.to));
+
+    void (await Promise.all(mots));
+
+    // for (const boid of boids) {
+    //   void (await boid.pos.set(boid.to, { duration: 500 }));
+    // }
   }
 
-  function* infiniteRandomIntegerFeed() {
+  function* infiniteRandomIntegerFeed(): Generator<
+    number | Promise<unknown>,
+    void,
+    unknown
+  > {
     while (true) {
       const randomInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER); // Generates a random integer
       yield randomInteger;
@@ -162,8 +141,15 @@
       console.log(value);
     }
   }
+  // consumeFeed();
 
-  consumeFeed();
+  function* finiteRandomIntegerGenerator() {
+    let count = 0;
+    while (count < 5) {
+      yield Math.floor(Math.random() * Number.MAX_SAFE_INTEGER); // Yield a random integer
+      count++;
+    }
+  }
 </script>
 
 <svelte:head>
@@ -173,29 +159,21 @@
 <div class="main">
   <div class="board">
     <svg width="400" height="300" viewBox="0 0 400 300">
-      <!-- {#each Object.values(rectSegs) as s}
-        <line
-          x1={greenSegw.from.current.x}
-          y1={greenSegw.from.current.y}
-          x2={greenSegw.to.current.x}
-          y2={greenSegw.to.current.y}
-          stroke="green"
-          stroke-width="2"
-        ></line>
-      {/each} -->
-
       <rect
-        x={thatRect.x}
-        y={thatRect.y}
-        width={thatRect.width}
-        height={thatRect.height}
+        x={container.x}
+        y={container.y}
+        width={container.width}
+        height={container.height}
         fill="none"
         stroke="blue"
         stroke-width="1"
-      ></rect>
-
+      >
+      </rect>
       {#each boids as b}
         <circle cx={b.pos.current.x} cy={b.pos.current.y} r={b.rad} />
+        <text x={b.pos.current.x} y={b.pos.current.y} class="small">
+          ({b.pos.current.x + 8}, {b.pos.current.y + 8})
+        </text>
       {/each}
       <line
         x1={greenSegw.from.current.x}
@@ -208,7 +186,7 @@
     </svg>
   </div>
   <nav class="button-bar">
-    <button onclick={randomOne}>Random</button>
+    <button onclick={advance}>Random</button>
   </nav>
 </div>
 
@@ -225,6 +203,10 @@
     height: 400px;
     width: 600px;
   }
+  .small {
+    font: italic 9px sans-serif;
+  }
+
   @media screen and (min-width: 576px) {
     /* landscape phones */
   }
