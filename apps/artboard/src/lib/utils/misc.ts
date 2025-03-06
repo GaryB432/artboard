502c594a6fc3e91f284888ca904b7ef8cd0a4992
Gary Bortosky <garyb432@users.noreply.github.com>
2025-03-06 13:30:29 -0600

    add array util

--- apps/artboard/src/lib/utils/misc.ts
+++ apps/artboard/src/lib/utils/misc.ts
@@ -25,6 +25,12 @@ export function shuffle<T>(array: T[]): T[] {
   return array;
 }
 
+export function getRandomElementExcluding<T>(array: T[], exclude: T[]): T {
+  return getRandomElement(
+    array.filter((a) => exclude.length === 0 || !exclude.includes(a)),
+  );
+}
+
 export function getRandomElement<T>(array: T[]): T {
   if (array.length === 0) {
     throw new Error("empty array");
